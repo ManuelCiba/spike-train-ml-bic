132cee0f416a69faa2f467621f19f83090de0e46
ManuelCiba <45418411+ManuelCiba@users.noreply.github.com>
2025-05-05 17:17:52 +0200

    Clean up comments

--- settings.py
+++ settings.py
@@ -3,16 +3,7 @@ import quantities as pq
 
 from old.settings import CONNECTIVITY_METHODS
 
-# KAI
 PATH_RESULTS_FOLDER = os.path.join(os.getcwd(), "results")
-
-# Macbook local
-#PATH_RESULTS_FOLDER = "/home/mc/Documents/Data/Japan/results"
-# Macbook Faubox
-#PATH_RESULTS_FOLDER = "/home/mc/FAUbox/Work/Projects/2023-10_BIC_Japan/PYTHON/BIC-Japan_macBook/pythonProject/results"
-
-# THAB
-#PATH_RESULTS_FOLDER = "/home/manuel/Documents/Data/Japan/results"
 
 # Folder names for the results
 FOLDER_NAME_split_data = "00_0_split_data"
@@ -36,23 +27,13 @@ FOLDER_NAME_features_all = "03_0_feature-set_all"
 
 # Parameter which will be calculated
 #BIN_SIZES = [10 * pq.ms]
-BIN_SIZES = [1 * pq.ms, 10 * pq.ms, 100 * pq.ms] # 5 * pq.ms
-#WINDOW_SIZES = [240 * pq.s]
+BIN_SIZES = [1 * pq.ms, 10 * pq.ms, 100 * pq.ms] 
 WINDOW_SIZES = [60 * pq.s, 120 * pq.s, 240 * pq.s]  # 30 s excluded, took too long.  600 * pq.s excluded, not enough data points for 10 fold cross validation
-#WINDOW_OVERLAPS = [0.75]
-WINDOW_OVERLAPS = [0, 0.25, 0.5, 0.75] # 0.5 0.5 = half window size
-#CONNECTIVITY_METHODS = ["spearman"]
+WINDOW_OVERLAPS = [0, 0.25, 0.5, 0.75] # 0.5 = 50% overlap
 CONNECTIVITY_METHODS = ["spearman", "canonical", "pearson"]  # tspe excluded, because needs another thresholding method than the other correlation methods
-FEATURE_SET_LIST = [#FOLDER_NAME_features_matrices,
-                    #FOLDER_NAME_features_measures,
-                    #FOLDER_NAME_features_measures_independent,
+FEATURE_SET_LIST = [
                     FOLDER_NAME_features_measures_synchrony_value,
                     FOLDER_NAME_features_measures,
-                    #FOLDER_NAME_features_measures_synchrony_value_independent,
-                    #FOLDER_NAME_features_measures_synchrony_curve,
-                    #FOLDER_NAME_features_synchrony_curve,
-                    #FOLDER_NAME_features_matrices_synchrony_curve
     ]
 
-#ML_MODELS = ['SVM', 'LR']
 ML_MODELS = ['RF', 'XGboost', 'SVM', 'NB', 'LR', 'KNN', 'MLP']
